8a95ae8a88cb6d6a3849332dc51dcd53b6b0d1b0
Bruno Francisco <bruno.f.c@icloud.com>
2023-02-11 10:52:17 -0300

    Quiz upado
    upload dos arquivos do quiz

---
+++ js/script.js
@@ -0,0 +1,67 @@
+var array = [];
+var lider, nerd, fundao, dorme, popular, result, quizURL = window.location.href;
+
+
+document.querySelectorAll('.card ol a').forEach(resposta =>{
+    resposta.addEventListener('click',function(){
+
+        array.push(resposta.getAttribute('id')); array.concat(resposta.getAttribute('id'))
+        
+        lider = array.filter(lider => lider === 'lider').length;
+        nerd = array.filter(nerd => nerd === 'nerd').length;
+        fundao = array.filter(dofundao => dofundao === 'dofundao').length;
+        dorme = array.filter(doquedormenacarteira => doquedormenacarteira === 'doquedormenacarteira').length;
+        popular = array.filter(popular => popular === 'popular').length;
+
+        resposta.closest('div').classList.add(resposta.closest('div').getAttribute('id'))
+        resposta.closest('div').style.translate = '-100% 0%';
+
+        setTimeout(function(){
+            if(resposta.closest('div').getAttribute('class').split(' ')[1] == resposta.closest('div').getAttribute('id'))
+            resposta.closest('div').style.display = 'none';
+        },300)
+                  if (lider >= 3 && array.length > 5 ){result = 'lider'; localStorage.setItem('resultado',result)
+            }else if (nerd >= 3 && array.length > 5){result = 'nerd'; localStorage.setItem('resultado',result)
+            }else if (fundao >= 3 && array.length > 5){result = 'dofundao'; localStorage.setItem('resultado',result)
+            }else if (dorme >= 3 && array.length > 5){result =  'doquedormenacarteira'; localStorage.setItem('resultado',result)
+            }else if (popular >= 3 && array.length > 5){result = 'popular'; localStorage.setItem('resultado',result)
+            }else if (array.length > 5) {result = 'vainaonda' }
+            else{ return true }
+        window.location = `https://kabum.com.br/hotsite/voltaasaulaslogitech-quiz/${result}.html`
+        // window.location = `${result}.html`
+    })
+    
+})
+document.querySelectorAll('#pergunta-1 a').forEach(item =>{
+    item.onclick = () =>{
+        document.querySelector('body').classList.toggle('visible')
+    }
+})
+document.querySelectorAll('#pergunta-2 a').forEach(item =>{
+    item.onclick = () =>{
+        document.querySelector('body').classList.toggle('visible')
+    }
+})
+function listaAleatoria() {
+    var ol = document.querySelectorAll('.card ol')
+    ol.forEach(ol =>{
+        var li = ol.children, 
+        lista = Array.from(li), 
+        sortear = lista.sort(() => Math.random() - 0.5);
+        ol.innerHTML = '';
+        sortear.forEach((node) => {
+            ol.appendChild(node);
+        });
+    })
+}
+document.querySelector('#startQuiz').addEventListener('click',function(){
+    document.querySelector('main').style.scale = '0'
+
+    setTimeout(function(){ document.querySelector('main').style.display = 'none'},200)
+
+    document.querySelector('#quiz').style.display = 'block';
+    listaAleatoria()
+})
+icnParallax = new Parallax(document.querySelector('main .icones'))
+icnParallax2 = new Parallax(document.querySelector('section .icones'))
+
---
+++ js/shr.js
@@ -0,0 +1,29 @@
+var quizURL = window.location.href
+var result = quizURL.substring(quizURL.lastIndexOf('/') + 1).replace('.html','')
+document.querySelector('#ogurl').setAttribute('content','https://kabum.com.br/hotsite/voltaasaulaslogitech-quiz/');
+
+fetch(`${quizURL.replace(result+'.html','')}resultado.json`).then(response => response.json()).then(json =>{
+        document.querySelector('#tt').setAttribute('href',`https://twitter.com/intent/tweet?url=https://kabum.com.br/hotsite/voltaasaulaslogitech-quiz/${json.resultado[result].url}&text=${json.resultado[result].shr}`)
+        console.log(document.querySelector('#tt'))
+      })
+
+    function resultados(){
+      fetch('resultado.json').then(response => {
+          return response.json()
+        }).then(json =>{
+          document.querySelector('.selo h3').innerHTML = `${json.resultado[result].titulo}`;
+          document.querySelector('.pBox').innerHTML = `${json.resultado[result].purpleBox}`;
+          document.querySelector('.text').innerHTML = `${json.resultado[result].texto}`;
+          document.querySelector('div.cta p').innerHTML = `${json.resultado[result].logitech}`
+          document.querySelector('.personagem img').setAttribute('src',`img/${json.resultado[result].char.replace('.webp','')}.webp`)
+        })
+        if(result.length > 7){document.querySelector('.selo h3').style.fontSize = '1.2rem'}else if(result.length > 10 ){document.querySelector('.selo h3').style.fontSize = '1.2rem'}else{return true}
+  }resultados()
+
+  document.querySelector('body').classList.toggle('bg')
+
+  console.log(localStorage);
+
+if(localStorage.getItem('resultado') == '' || localStorage.getItem('resultado') == "undefined" || localStorage.getItem('resultado') == null){
+  window.location = 'https://kabum.com.br/hotsite/voltaasaulaslogitech-quiz'
+}
